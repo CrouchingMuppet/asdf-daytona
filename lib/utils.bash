4553e8713d4d0c8569fdb5295f034f08b7f993a4
Miles Parfitt <miles.parfitt@icloud.com>
2025-08-11 15:51:04 +1000

    Get cli versions from Daytona's public s3

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-GH_REPO="https://github.com/daytonaio/daytona"
 TOOL_NAME="daytona"
 TOOL_TEST="daytona version"
 
@@ -13,25 +12,15 @@ fail() {
 
 curl_opts=(-fsSL)
 
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//'
-}
-
 list_all_versions() {
-  # CLI releases do not correspond with daytona releases on Github
-  # Hardcoding this for now
-  echo "0.16.11"
+  curl "${curl_opts[@]}" "https://daytona-workspace-tools.s3.us-east-1.amazonaws.com/?list-type=2&prefix=cli/&delimiter=%2F" |
+    grep -o '<Prefix>cli/[0123456789.]*/</Prefix>' |
+    cut -d/ -f2
 }
 
 download_release() {
